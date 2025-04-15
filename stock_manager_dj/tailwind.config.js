a5b54fd6a3e63443b9f42882051d36be73c0228f
KauanRTe <kauan065@hotmail.com>
2025-04-15 14:09:50 -0300

    Reajuste na template base e tailwindcss

--- stock_manager_dj/tailwind.config.js
+++ stock_manager_dj/tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   content: [
     "./templates/**/*.html",
+    "./**/templates/**/*.html",
     "./static/**/*.{html,js}",
     "./static/css/**/*.css"
+  ],
+  safelist: [
+    'bg-black',
+    'text-white',
+    'hover:bg-red-500',
   ],
   theme: {
     extend: {},
